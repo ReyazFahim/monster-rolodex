fab23d59f1276e50fa6cd597293d02f0b8edcb7d
ReyazFahim <reyaz4u1992@gmail.com>
2020-04-17 00:04:03 +0530

    Updates

---
+++ precache-manifest.50e6b084ba049b752771f8753dbfabeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb9bcfe6dc5f3ab1a0770dca6fc0ece",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c582ed864d7b34e38261",
+    "url": "/monster-rolodex/static/css/main.b417097f.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monster-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c582ed864d7b34e38261",
+    "url": "/monster-rolodex/static/js/main.e414945f.chunk.js"
+  },
+  {
+    "revision": "e4eba3394aec38fdb39c",
+    "url": "/monster-rolodex/static/js/runtime-main.2047114a.js"
+  }
+]);
